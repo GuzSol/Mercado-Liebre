1db7f6a2f976caf75fa620db342f50c6f2081477
Guzmán <arq.gsoler@gmail.com>
2023-07-18 00:08:31 -0300

    listo para subir a render (intento3)

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const path = require ("path");
 const dotenv = require ("dotenv").config();
+
 const app = express();
 
 const publicPath = path.resolve(__dirname, "./public");
+
 app.use( express.static(publicPath));
 
-process.env.PORT, () => {
-    console.log("Servidor corriendo en el puerto 3000");
-};
 
 app.get("/", (req, res) => {
     res.sendFile (path.resolve(__dirname, "./views/home.html"));
@@ -25,6 +24,29 @@ app.get('/login', (req, res) => {
 /* app.listen (3000, () =>
 console.log ("Servidor corriendo")); */
 
+/* process.env.PORT, () => {
+    console.log("Servidor corriendo en el puerto 3000");
+}; */
 
 app.listen (process.env.PORT, () =>{
     console.log("servidor corriendo en el puerto ${port}");});
+
+
+ /* saqué de mi package.json
+     {
+        "name": "m4-mercado-liebre",
+        "version": "1.0.0",
+        "description": "ejercicio",
+        "main": "app.js",
+        "scripts": {
+          "test": "nodemon src/index.js",
+          "start": "node src/index.js"
+        },
+        "author": "Guz",
+        "license": "ISC",
+        "dependencies": {
+          "dotenv": "^16.3.1",
+          "express": "^4.18.2"
+        }
+      }
+ */      
